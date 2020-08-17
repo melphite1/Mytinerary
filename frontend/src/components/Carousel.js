974ce37982f52bf7e8c634546e75a2e265e0704a
cris <cristiansuarez95@hotmail.com>
2020-08-17 19:38:55 -0300

    cambio carousel

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -3,74 +3,31 @@ import Carousel from 'react-bootstrap/Carousel'
 import "../styles/Carousel.css"
 
 function Carousel1() {
+    const ciudades = [
+        ["London", "Chicago", "LosAngeles", "NuevaYork"],
+        ["Osaka", "Paris", "SaoPaulo", "Seul"],
+        ["Shangai", "Taipei", "Tokio", "Yakarta"]
+    ]
 
-
-    const ciudades1 = ["London", "Chicago", "LosAngeles", "NuevaYork"]
-    const ciudades2 = ["Osaka", "Paris", "SaoPaulo", "Seul"]
-    const ciudades3 = ["Shangai", "Taipei", "Tokio", "Yakarta"]
     return (
 
         <Carousel className="carousel" interval={2000}>
 
-            <Carousel.Item>
+            {ciudades.map(conjunto => {
+                return (
+                    <Carousel.Item>
+                        <div id="contenedorCarr">
+                            {conjunto.map(ciudad => {
+                                const fotoCiudad = require(`../imagenes/ImagLugares/${ciudad}.jpg`)
+                                return (
 
-                <div id="contenedorCarr">
-                    {ciudades1.map(ciudad => {
-                        const fotoCiudad = require(`../imagenes/ImagLugares/${ciudad}.jpg`)
-                        return (
-                            <>
-                                <div
-                                    id="contenedorImg"
-                                    style={{ backgroundImage: `url(${fotoCiudad})`, display: "flex", alignItems: "center", fontSize:"45px" }}
-                                >            <div id="nombreCiudad"><p style={{ margin: "0", padding: "0", color: "#F1F3AE" }}>{ciudad}</p></div></div>
-
-
-                            </>)
-                    })}
-                </div>
-
-
-
-
-            </Carousel.Item>
-            <Carousel.Item>
-            <div id="contenedorCarr">
-                    {ciudades2.map(ciudad => {
-                        const fotoCiudad = require(`../imagenes/ImagLugares/${ciudad}.jpg`)
-                        return (
-                            <>
-                                <div
-                                    id="contenedorImg"
-                                    style={{ backgroundImage: `url(${fotoCiudad})`, display: "flex", alignItems: "center", fontSize:"45px" }}
-                                >            <div id="nombreCiudad"><p style={{ margin: "0", padding: "0", color: "#F1F3AE" }}>{ciudad}</p></div></div>
-
-
-                            </>)
-                    })}
-                </div>
-
-
-
-            </Carousel.Item>
-            <Carousel.Item>
-
-            <div id="contenedorCarr">
-                    {ciudades3.map(ciudad => {
-                        const fotoCiudad = require(`../imagenes/ImagLugares/${ciudad}.jpg`)
-                        return (
-                            <>
-                                <div
-                                    id="contenedorImg"
-                                    style={{ backgroundImage: `url(${fotoCiudad})`, display: "flex", alignItems: "center", fontSize:"45px" }}
-                                >            <div id="nombreCiudad"><p style={{ margin: "0", padding: "0", color: "#F1F3AE" }}>{ciudad}</p></div></div>
-
-
-                            </>)
-                    })}
-                </div>
-
-
-            </Carousel.Item>
+                                    <div id="contenedorImg" style={{ backgroundImage: `url(${fotoCiudad})`, display: "flex", alignItems: "center", fontSize: "45px" }}>   <div id="nombreCiudad"><p style={{ margin: "0", padding: "0", color: "#F1F3AE" }}>{ciudad}</p></div></div>
+                                )
+                            })}
+                        </div>
+                    </Carousel.Item>
+                )
+            })}
 
 
 
